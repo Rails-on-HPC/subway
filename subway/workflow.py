e5eb0b6aecb6727c8caf357a9cc1856c3e8decbf
refraction-ray <kcanamgal@foxmail.com>
2020-02-25 20:31:04 +0800

    try action with multiple os

--- subway/workflow.py
+++ subway/workflow.py
@@ -1,9 +1,14 @@
 import time
 
 
-def main_once(
-    checker, submitter
-):  ## preprocesser is once for all and hence not include in main
+def main_once(checker, submitter):
+    """
+    main entry point run checker and submitter once.
+
+    :param checker: Checker.
+    :param submitter: Submitter.
+    :return: None
+    """
 
     ######
     # check part
@@ -22,12 +27,19 @@ def main_once(
 
 def main_rt(checker, submitter, sleep_interval=10, loops=None):
     """
-    
-    :param checker:
-    :param submitter:
-    :param sleep_interval:
-    :param loops:
-    :return:
+    realtime main monitor for subway entry_point
+
+    :param checker: Checker.
+    :param submitter: Submitter.
+    :param sleep_interval: Optional(Union(float, Tuple(float, float))), default 10.
+                There are two intervals, one after checker, one after submitter.
+                If one number is given, then two intervals are the same.
+                If tuple or list of 2 numbers are given, they are explained as
+                (after_checker_interval, after_submitter_interval)
+    :param loops: Optional(int), default None.
+                max loops before exit, none for running forever,
+                but the monitor can still quit if no job is running as expected.
+    :return: None
     """
     # TODO: better way for realtime always-on monitor version of main?
     # naive version
